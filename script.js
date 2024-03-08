9c759bb4879e449c9246b779ccda69c29103a7ca
satork <sebastian.kedzier@gmail.com>
2024-03-08 15:15:57 +0100

    stylization for dialog modal

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+const openModalButton = document.querySelector("#openModal");
+const closeModalButton = document.querySelector("#closeModal");
+const addModalButton = document.querySelector("#addModal");
+const dialog = document.getElementById("modal");
+
+openModalButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeModalButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+addModalButton.addEventListener("click", (event) => {
+    event.preventDefault();
+})
+
 const book1 = new Book("aaa", "bbb", 123);
 const myLibrary = new Library();
 
-myLibrary.addBooks(book1)
+myLibrary.addBooks(book1);
 
 console.log(myLibrary);
